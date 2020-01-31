74efab92c40059807e2fb15772f2d75bba5c0866
cedricLafumat <cedricLafumat@le-campus-numerique.fr>
2020-01-31 08:28:16 +0100

    Mise en place d'une requête de lecture de la table plante dans le fichier read.py et une requête d'insert dans le fichier insert.py

--- insert.py
+++ insert.py
@@ -1,1 +1,22 @@
-print("hello world")
+import mysql.connector
+db = mysql.connector.connect(user='cedric', password='Moustic26*',
+                              host='localhost',
+                              database='herbalist')
+
+cursor = db.cursor()
+
+
+def main():
+    id_record = int(input("Saisissez l'id de votre enregistrement : "))
+    name_record = input("Saisissez un nom : ")
+    indication_record = input("Saisissez une indication : ")
+    used_piece_record = input("Saisissez la partie utilisée : ")
+    price_record = input("Saisissez le prix : ")
+
+
+    cursor.execute("INSERT INTO plante (id, nom, indication, partie_utilisee, prix) VALUES ('{}','{}','{}','{}','{}')".format(id_record, name_record, indication_record, used_piece_record, price_record))
+    db.commit()
+
+
+if __name__ == '__main__':
+    main()
--- read.py
+++ read.py
@@ -1,1 +1,13 @@
-print("hello world")
+import mysql.connector
+db = mysql.connector.connect(user='cedric', password='Moustic26*',
+                              host='localhost',
+                              database='herbalist')
+
+cursor = db.cursor()
+
+cursor.execute("SELECT * FROM plante")
+
+result = cursor.fetchall()
+
+for record in result:
+    print(record)
